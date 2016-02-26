85119f38b295a72f1088e3eb2fe76b87c2d215ea
drkitty <bumblingkitty@gmail.com>
2016-02-26 11:41:49 -0800

    Get a basic lexer working

---
+++ assembler/common.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#define E_COMMON 1
+#define E_RARE 2
+
+
+#define print(x) fputs((x), stdout)
--- assembler/test.re.c
+++ assembler/test.re.c
@@ -1,49 +1,117 @@
+#include "common.h"
+
 #include <stdbool.h>
 #include <stdio.h>
+
+#include "fail.h"
 
 
 #define endof(p) ((char *)p + sizeof(*p))
 
-#define T_EOF 0
-#define T_A 1
-#define T_Z 2
+enum toktype {
+    T_EOF,
+    T_CHAR,
+    T_TEXT,
+    T_NUM,
+};
 
 
-void YYFILL(int n)
+struct lexer {
+    char *cursor;
+    char *limit;
+};
+
+
+struct token {
+    enum toktype type;
+    char* s;
+    int n;
+};
+
+
+void print_token(struct token* t)
+{
+    switch (t->type) {
+    case T_EOF: print("T_EOF"); break;
+    case T_CHAR: printf("T_CHAR(%d)", t->n); break;
+    case T_TEXT: {
+        char c = t->s[t->n];
+        t->s[t->n] = '\0';
+        printf("T_TEXT(\"%s\")", t->s);
+        t->s[t->n] = c;
+        break;
+    }
+    case T_NUM: printf("T_NUM(%d)", t->n); break;
+    default: fatal(E_RARE, "BUG");
+    }
+}
+
+
+void _lexer_fill(int n)
 {
     (void)n;
 }
 
 
-#define YYCURSOR *cursor
-#define YYCTYPE char
-int scan(char** cursor, char* YYLIMIT)
+struct token lexer_next(struct lexer* lexer)
 {
-    /*static char* YYMARKER;*/
+    struct token t;
 
-/*!re2c
+    while (true) {
+        char* start = lexer->cursor;
+        /*!re2c
+            re2c:define:YYFILL = "_lexer_fill";
+            re2c:define:YYCTYPE = "char";
+            re2c:define:YYCURSOR = "lexer->cursor";
+            re2c:define:YYLIMIT = "lexer->limit";
 
-   "a" { return T_A; }
-   "z" { return T_Z; }
-   "\x00" { return T_EOF; }
+            "\n" {
+                t.type = T_CHAR;
+                t.n = '\n';
+                return t;
+            }
 
-*/
+            [a-zA-Z_][0-9a-zA-Z_]* {
+                t.type = T_TEXT;
+                t.s = start;
+                t.n = lexer->cursor - start;
+                return t;
+            }
 
+            [1-9][0-9]* {
+                t.type = T_NUM;
+                t.s = start;
+                t.n = lexer->cursor - start;
+                return t;
+            }
+
+            [ \t]+ { continue; }
+
+            "\x00" {
+                t.type = T_EOF;
+                return t;
+            }
+
+            * { fatal(E_COMMON, "Invalid character"); }
+        */
+    }
 }
-#undef YYCURSOR
-#undef YYCTYPE
 
 
 int main()
 {
-    char buf[] = "aazazz\x00";
+    char buf[] = "abc ABC\n\n\n_0189_\n    _ _ _  \n\na1a1\x00";
+    struct lexer lexer = {
+        .cursor = buf,
+        .limit = endof(buf) - 1,
+    };
 
-    char* c = buf;
     while (true) {
-        int t = scan(&c, endof(buf) - 1);
-        if (t == T_EOF)
+        struct token t = lexer_next(&lexer);
+        if (t.type == T_EOF)
             break;
-        printf("%d\n", t);
+        print_token(&t);
+        putchar('\n');
     }
 
     return 0;
