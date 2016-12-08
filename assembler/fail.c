393f1fb3aa437ac6164e9bda6b31873eaaa4194f
drkitty <bumblingkitty@gmail.com>
2016-12-07 21:05:38 -0800

    Update fail.*

--- assembler/fail.c
+++ assembler/fail.c
@@ -8,61 +8,66 @@
 
 void vx_(const char* srcname, int line, const char* format, ...)
 {
-    va_list args;
-    va_start(args, format);
     fflush(stdout);
 #ifdef DEBUG
     fprintf(stdout, "%s:%d: ", srcname, line);
 #else
     (void)srcname; (void)line;
 #endif
+    va_list args;
+    va_start(args, format);
     vfprintf(stdout, format, args);
+    va_end(args);
     putchar('\n');
 }
 
 
 void warning_(const char* srcname, int line, const char* format, ...)
 {
-    va_list args;
-    va_start(args, format);
     fflush(stdout);
 #ifdef DEBUG
     fprintf(stderr, "%s:%d: ", srcname, line);
 #else
     (void)srcname; (void)line;
 #endif
+    va_list args;
+    va_start(args, format);
     vfprintf(stderr, format, args);
+    va_end(args);
     putc('\n', stderr);
 }
 
 
 void warning_e_(const char* srcname, int line, const char* format, ...)
 {
-    va_list args;
-    va_start(args, format);
+    int e = errno;
     fflush(stdout);
 #ifdef DEBUG
     fprintf(stderr, "%s:%d: ", srcname, line);
 #else
     (void)srcname; (void)line;
 #endif
+    va_list args;
+    va_start(args, format);
     vfprintf(stderr, format, args);
-    fprintf(stderr, " (%s)\n", strerror(errno));
+    va_end(args);
+    fprintf(stderr, " (%s)\n", strerror(e));
 }
 
 
 void fatal_(int rtn, const char* srcname, int line,
         const char* format, ...)
 {
-    va_list args;
-    va_start(args, format);
     fflush(stdout);
 #ifdef DEBUG
     fprintf(stderr, "%s:%d: ", srcname, line);
 #else
     (void)srcname; (void)line;
 #endif
+    va_list args;
+    va_start(args, format);
     vfprintf(stderr, format, args);
+    va_end(args);
     putc('\n', stderr);
     exit(rtn);
 }
@@ -71,15 +76,17 @@ void fatal_(int rtn, const char* srcname, int line,
 void fatal_e_(int rtn, const char* srcname, int line,
         const char* format, ...)
 {
-    va_list args;
-    va_start(args, format);
+    int e = errno;
     fflush(stdout);
 #ifdef DEBUG
     fprintf(stderr, "%s:%d: ", srcname, line);
 #else
     (void)srcname; (void)line;
 #endif
+    va_list args;
+    va_start(args, format);
     vfprintf(stderr, format, args);
-    fprintf(stderr, " (%s)\n", strerror(errno));
+    va_end(args);
+    fprintf(stderr, " (%s)\n", strerror(e));
     exit(rtn);
 }
